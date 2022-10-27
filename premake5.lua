b07ed85592a682310b5ac246988f784e57f75728
LikePoetry <249857765@qq.com>
2022-10-27 10:27:19 +0800

    初始化项目结构
    premake5.lua 文件编写，并生成项目！

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "excimer"
+    architecture "x64"
+    startproject "editor"
+
+    configurations{
+        "Debug"
+    }
+
+    outputdir="%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+group ""
+
+--静态链接库，核心文件
+project "excimer"
+	location "excimer"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	characterset ("MBCS")
+
+	targetdir("bin/" ..outputdir.. "/%{prj.name}")
+	objdir("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	pchheader "hzpch.h"
+	pchsource "excimer/src/hzpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%VULKAN_SDK%/include"
+	}
+
+	libdirs 
+	{ 
+		"%VULKAN_SDK%/lib" 
+	}
+
+	links
+	{
+		"vulkan-1.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			GLFW_INCLUDE_NONE
+		}
+
+
+	filter "configurations:Debug"
+		defines ""
+		runtime "Debug"
+		symbols "on"
+
+-- 测试工具项目
+project "editor"
+    location "editor"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	characterset ("MBCS")
+
+	targetdir("bin/" ..outputdir.. "/%{prj.name}")
+	objdir("bin-int/" ..outputdir.. "/%{prj.name}")
+	
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+	{
+        "excimer/src",
+        "%VULKAN_SDK%/include",
+    }
+
+    links
+    {
+        "excimer",
+        "vulkan-1.lib"
+    }
+
+    libdirs 
+	{ 
+		"%VULKAN_SDK%/lib" 
+	}
+
+    filter "system:windows"
+		systemversion "latest"
+
+    filter "configurations:Debug"
+		defines ""
+		runtime "Debug"
+		symbols "on"    
---
+++ scripts/GenerateProjects.bat
@@ -0,0 +1,4 @@
+cd ../
+call scripts\premake\premake5.exe vs2019
+
+pause
