6aae6afcb9ab59c2e735d9d768b03b89a340c720
LikePoetry <249857765@qq.com>
2022-11-08 17:55:24 +0800

    update framebuffer！

--- excimer/src/excimer/graphics/rhi/Framebuffer.cpp
+++ excimer/src/excimer/graphics/rhi/Framebuffer.cpp
@@ -2,3 +2,13 @@
 #include "Framebuffer.h"
 #include "Texture.h"
 #include "RenderPass.h"
+
+#include "excimer/platform/vulkan/VK.h"
+
+namespace Excimer
+{
+	namespace Graphics
+	{
+
+	}
+}
--- excimer/src/excimer/graphics/rhi/Framebuffer.h
+++ excimer/src/excimer/graphics/rhi/Framebuffer.h
@@ -32,7 +32,31 @@ namespace Excimer
 
 		class EXCIMER_EXPORT Framebuffer
 		{
+		public:
+			static SharedPtr<Framebuffer> Get(const FramebufferDesc& framebufferDesc);
+			static Framebuffer* Create(const FramebufferDesc& framebufferDesc);
+			static void ClearCache();
+			static void DeleteUnusedCache();
 
+			virtual ~Framebuffer();
+
+			virtual void Bind(uint32_t width, uint32_t height) const = 0;
+			virtual void Bind() const = 0;
+			virtual void UnBind() const = 0;
+			virtual void Clear() = 0;
+			virtual void Validate() {};
+			virtual void AddTextureAttachment(RHIFormat format, Texture* texture) = 0;
+			virtual void AddCubeTextureAttachment(RHIFormat format, CubeFace face, TextureCube* texture) = 0;
+			virtual void AddShadowAttachment(Texture* texture) = 0;
+			virtual void AddTextureLayer(int index, Texture* texture) = 0;
+			virtual void GenerateFramebuffer() = 0;
+
+			virtual uint32_t GetWidth() const = 0;
+			virtual uint32_t GetHeight() const = 0;
+			virtual void SetClearColour(const glm::vec4& colour) = 0;
+
+		protected:
+			static Framebuffer* (*CreateFunc)(const FramebufferDesc&);
 		};
 	}
 }
