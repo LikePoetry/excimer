3f49a465bcadeda83aa900ccf67cb0643ddeb0a7
LikePoetry <249857765@qq.com>
2022-11-09 11:31:58 +0800

    添加 engine

--- excimer/src/excimer/core/Engine.cpp
+++ excimer/src/excimer/core/Engine.cpp
@@ -1,2 +1,16 @@
 #include "hzpch.h"
 #include "Engine.h"
+
+namespace Excimer
+{
+    Engine::Engine()
+        : m_MaxFramesPerSecond(1000.0f / 60.0f)
+    {
+        m_TimeStep = new TimeStep();
+    }
+
+    Engine::~Engine()
+    {
+        delete m_TimeStep;
+    }
+}
